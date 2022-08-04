0ad130b0c5c1629bc480c4bbf7c724d59e829fc6
Takim <93022529+takimtod@users.noreply.github.com>
2022-08-04 21:28:16 +0800

    Add files via upload

--- kimmy.js
+++ kimmy.js
@@ -5637,7 +5637,7 @@ title: 'play music',
 body: 'Now Playing...',
 description: 'Now Playing...',
 mediaType: 2,
-thumbnail: await (await fetch('https://telegra.ph/file/5ad51566ce5ae7ac710b7.jpg')).buffer(),
+thumbnail: `${q}`,
 mediaUrl: `${q}`
 }}}, { quoted: m })
             }
@@ -5648,7 +5648,7 @@ case 'ytmp4': case 'ytvideo': {
 		db.users[sender].limit -= 1 // -1 limit
 let { ytv } = require('./lib/y2mate')
 if (!text) throw `Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 360p`
-let quality = args[1] ? args[1] : '360p'
+let quality = args[1] ? args[1] : '720p'
 let media = await ytv(text, quality)
 if (media.filesize >= 100000) return reply('File Melebihi Batas '+util.format(media))
 aqua.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `➟ Title : ${media.title}\n➟ File Size : ${media.filesizeF}\n➟ Url : ${isUrl(text)}\n➟ Ext : MP3\n➟ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
