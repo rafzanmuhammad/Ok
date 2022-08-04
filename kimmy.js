f4c75a2e9ea3f85ac44ea01d5a8a51887a60df45
Takim <93022529+takimtod@users.noreply.github.com>
2022-08-04 21:33:12 +0800

    Add files via upload

--- kimmy.js
+++ kimmy.js
@@ -2301,7 +2301,7 @@ File Size : ${nana.filesizeH}
 Upload : ${nana.aploud}
 `
  reply (tuks)
-if(nana.filesize > 90000){
+if(nana.filesize > 500000){
 return reply ("File size melebihi batas,\nbatas yang di tentukan adalah 90mb")              
 }
  aqua.sendMedia2 (from, nana.url, m, {fileName: nana.filename})
@@ -5650,7 +5650,7 @@ let { ytv } = require('./lib/y2mate')
 if (!text) throw `Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 360p`
 let quality = args[1] ? args[1] : '720p'
 let media = await ytv(text, quality)
-if (media.filesize >= 100000) return reply('File Melebihi Batas '+util.format(media))
+if (media.filesize >= 400000) return reply('File Melebihi Batas '+util.format(media))
 aqua.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `➟ Title : ${media.title}\n➟ File Size : ${media.filesizeF}\n➟ Url : ${isUrl(text)}\n➟ Ext : MP3\n➟ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
             }
             break
