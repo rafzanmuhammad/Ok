8397943df4cbcc100fb4a30ac2d352c1c2e5e696
Takim <93022529+takimtod@users.noreply.github.com>
2022-08-20 14:58:23 +0800

    Update help.js

--- massege/help.js
+++ massege/help.js
@@ -1487,4 +1487,12 @@ return `➲ *Random Wallpaper*
  `}
  
  
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
+
  
