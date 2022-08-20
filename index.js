7ba1bab53c4b8a7af6697c7c2fdc06c8ddf9a000
Takim <93022529+takimtod@users.noreply.github.com>
2022-08-20 13:37:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/
 const { smsg, isUrl, getRandom, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/myfunc')
 const forward = { forwardingScore: 10000000, isForwarded: true, sendEphemeral: true}
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://aqua:JEuf6Bc#aVQFyW9@cluster0.b4kpf6s.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://kimtod:AKYQneA6S2DKuS@cluster0.p5h5oy6.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
