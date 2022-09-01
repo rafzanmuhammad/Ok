6f92d6f07b0a605b0ab0f3bcbf4a87e900962370
Takim <93022529+takimtod@users.noreply.github.com>
2022-09-01 09:13:44 +0800

    Add files via upload

--- kimmy.js
+++ kimmy.js
@@ -617,7 +617,7 @@ reply("Simi ga tau mau ngomong apa")
 
 //AUTO RESPON SIMI BY DECODE DENPA 
 //if (chatBot === false && autorespon === true && !isGroup && !isCmd && !command && !dev.key.fromMe) { 
-if (botNumber & isQuotedTag && isQuotedReply) {
+if (botNumber & isQuotedTag && isQuotedReply && command) {
 	try{
 aqua.sendPresenceUpdate('composing', from) 
 
@@ -628,7 +628,7 @@ const { findPhoneNumbersInText, parsePhoneNumber }= require('libphonenumber-js')
   console.log(idnya)
 let kata = body.includes(`@${botNumber.split("@")[0]}`, "")
 
-let simi = await fetchJson(`https://api.simsimi.net/v2/?text=${body}&lc=id`, {methods: "GET"})
+let simi = await fetchJson(`https://api.simsimi.net/v2/?text=${command}&lc=id`, {methods: "GET"})
 let sami = simi.success
 console.log(sami)
 aqua.sendMessage(from,{text: sami},{quoted: m})  
