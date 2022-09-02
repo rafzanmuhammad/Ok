347d7e7eb469d3f463fdcdb90d464753b7483852
Takim <93022529+takimtod@users.noreply.github.com>
2022-09-02 09:33:44 +0800

    Add files via upload

--- kimmy.js
+++ kimmy.js
@@ -48,7 +48,7 @@ const gcbt = ["120363041984430206@g.us"] //ID GC GRUB BOT "120363041984430206@g.
 const ameApi = new ameClient("1f486b04b157f12adf0b1fe0bd83c92a28ce768683871d2a390e25614150d0c8fa404fd01b82a5ebf5b82cbfa22e365e611c8501225a93d5d1e87f9f420eb91b")
 const bitly = new BitlyClient('7d737131e678fc366699edead8bca146e69f6c78', {});
 const forward = { forwardingScore: 10000000, isForwarded: true, sendEphemeral: true}
-const { youtubeSearch, mediafiredl,  lyricsv2,  lyrics, facebookdl, facebookdlv2, tiktokdl, tiktokdlv2, twitterdl, twitterdlv2, getZodiac, liputan6, googleIt, wallpaperv2,  googleImage,  jadwalTVNow,  gempa,  stickerTelegram, stickerLine, latinToAksara, aksaraToLatin, asmaulhusna, asmaulhusnajson, alquran, jadwalsholat, listJadwalSholat, gempaNow, instagramdl, instagramdlv3 } = require('@bochilteam/scraper')
+const { youtubeSearch, mediafiredl,  lyricsv2,  lyrics, facebookdl, facebookdlv2, tiktokdl, tiktokdlv2, twitterdl, twitterdlv2, getZodiac, liputan6, googleIt, wallpaperv2,  googleImage,  jadwalTVNow,  gempa,  stickerTelegram, stickerLine, latinToAksara, aksaraToLatin, asmaulhusna, asmaulhusnajson, alquran, jadwalsholat, listJadwalSholat, gempaNow, instagramdl, instagramdlv3, instagramdlv2 } = require('@bochilteam/scraper')
 const stringSimilarity = require("string-similarity");
 
 //=======================[ LIB ]=======================//
@@ -3639,6 +3639,13 @@ db.users[sender].limit -= 1 // -1 limit
 break
 
 
+case 'fbbdl':{
+reply(mess.wait)
+let { instagramdl, instagramdlv2, instagramdlv3, instagramStory, aiovideodl, savefrom, snapsave } = require('@bochilteam/scraper')
+let res = await savefrom(args[0])
+for (const { url } of res ) await aqua.sendMedia(from, url, m)
+}
+break
 
 case 'stalkig': case 'igstalk':{
 if (!q) return reply ('namany?')
