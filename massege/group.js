8de8b7a0df2789edcf1c4aefa62ff96a2d4e55ac
Takim <93022529+takimtod@users.noreply.github.com>
2022-09-04 13:11:11 +0800

    Add files via upload

--- massege/group.js
+++ massege/group.js
@@ -235,10 +235,10 @@ if(type1){
 WelcomeType1(from, intro, copyright, ppUser, ppGc, butIn,{ "mentionedJid": [mem]})
 } else if(type2){
 	
- aqua.sendMessage(from, { caption: intro, image: welcom, buttons: butIn, footer: 'tes welcome' })
+// aqua.sendMessage(from, { caption: intro, welcom, buttons: butIn, footer: 'tes welcome' })
 
 //aqua.sendMessage(from, { caption: intro, image: {url: `${welcom}`}, buttons: butIn, footer: 'tes welcome' })
-//WelcomeType2(from, intro, copyright, gmbr, butIn, { "mentionedJid": [mem]})
+WelcomeType2(from, intro, copyright, welcom, butIn, { "mentionedJid": [mem]})
 } else if(type3){
 sendButDoc(from, intro, `Rules:
 ${desc}`, ppUser, butIn, options1)
@@ -268,11 +268,11 @@ if(type1){
 WelcomeType1(from, outro,  `Hm Kok Keluar ^_^`, ppUser, ppGc, butOut, { "mentionedJid": [mem]})
 } else if(type2){
 
-aqua.sendMessage(from, { caption: outro, image: welcom, buttons: butIn, footer: 'tes welcome' })
+//aqua.sendMessage(from, { caption: outro, image: welcom, buttons: butIn, footer: 'tes welcome' })
 
 //aqua.sendMessage(from, { caption: outro, image: {url: "https://sewa4yeye.herokuapp.com/api/canvas/welcome?nama=${pushname}&namaGb=${groupName}&pepeGb=${ppGc}&totalMem=${allmem}&pepeUser=${ppUser}&bege=https://tinyurl.com/y23xrfhu&apikey=BetaBotz" ` `${welcom}`}, buttons: butOut, footer: 'tes welcome' })
 //aqua.sendMessage(from, {image: {url: welcom}, caption: outro, mentions:[mem]})
-//WelcomeType2(from, outro,  `Hm Kok Keluar ^_^`, gmbr, butOut, { "mentionedJid": [mem]})
+WelcomeType2(from, outro,  `Hm Kok Keluar ^_^`, welcom, butOut, { "mentionedJid": [mem]})
 } else if(type3){
 sendButDoc(from, outro, `Hm Kok Keluar ^_^`, ppUser, butOut, options1)
 } else if(type4){
