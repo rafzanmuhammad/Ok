040be915308f55de006313fe18cb733a93bd99ad
Takim <93022529+takimtod@users.noreply.github.com>
2022-09-17 00:19:49 +0800

    Update kimmy.js

--- kimmy.js
+++ kimmy.js
@@ -8049,7 +8049,7 @@ console.log(util.format(err))
 let e = String(err) 
 if (e.includes("this.isZero")) {return}
 if (e.includes('Connection Closed')){ return }
-if (e.includes('Timed Out')){ return }
+if (e.includes('Error R15')){ return }
 console.log(color('Message Error : %s', 'white'), color(util.format(e), 'green'))
 
 }
